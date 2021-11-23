095a62936b766824f8dceca90106d826c11d4e94
amelie darnat <darnat.amelie@gmail.com>
2021-11-23 13:36:59 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :time_slots, only: [:new, :create, :show, :index, :destroy] do
+    resources :bookings, only: [:new, :create, :show]
+  end
+
+  resources :users, only: [:user_profile] do
+    resources :sport_classes, only: [:show, :new, :create]
+  end
+
+  resources :sport_classes, only: [:destroy]
 end
