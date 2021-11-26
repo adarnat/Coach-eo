088f4e6ac7ad1f936521f50334cd9e726839a64d
amelie darnat <darnat.amelie@gmail.com>
2021-11-26 14:27:46 +0100

    home

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,8 @@
 class BookingsController < ApplicationController
+
+    # def home
+    # if current_user.present?
+    #   @bookings = Booking.includes(:sport_class).where(sport_classes: { coach_id: current_user.id }).where("start_at BETWEEN ? and ?", Date.current.beginning_of_day, Date.current.end_of_day)
+    # end
+  # end
 end
