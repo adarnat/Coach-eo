3c1c0b26add4a47468570fde7cae5b9e99396efd
amelie darnat <darnat.amelie@gmail.com>
2021-11-26 15:26:16 +0100

    try 6

--- app/javascript/init_calendar.js
+++ app/javascript/init_calendar.js
@@ -8,58 +8,58 @@ let coachEvents;
 let coachCalendar;
 let coachCalendarEl;
 
-// const eventCoachClick = (info) => {
-//   console.log("j'ai cliqué sur un event de coach calendar")
-//   console.log(info.event)
-//   $("#exampleModal").modal('show')
-// }
+const eventCoachClick = (info) => {
+  console.log("j'ai cliqué sur un event de coach calendar")
+  console.log(info.event)
+  $("#exampleModal").modal('show')
+}
 
-// const createCoachCalendar = () => {
-//   coachCalendar = new Calendar(coachCalendarEl, {
-//     timeZone: 'Europe/Paris',
-//     plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
-//     hiddenDays: [0],
-//     droppable: true,
-//     editable: true,
-//     dayMaxEventRows: true,
-//     navLinks: true,
-//     eventStartEditable: true,
-//     eventDurationEditable: true,
-//     initialView: 'dayGridMonth',
-//     selectable: true,
-//     headerToolbar: {
-//       left: 'prev,next today',
-//       center: 'title',
-//       right: 'dayGridMonth,timeGridWeek,listWeek'
-//     },
-//     events: coachEvents,
-//     eventClick: eventCoachClick
-//   });
+const createCoachCalendar = () => {
+  coachCalendar = new Calendar(coachCalendarEl, {
+    timeZone: 'Europe/Paris',
+    plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
+    hiddenDays: [0],
+    droppable: true,
+    editable: true,
+    dayMaxEventRows: true,
+    navLinks: true,
+    eventStartEditable: true,
+    eventDurationEditable: true,
+    initialView: 'dayGridMonth',
+    selectable: true,
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,listWeek'
+    },
+    events: coachEvents,
+    eventClick: eventCoachClick
+  });
 
-//   coachCalendar.render()
-// }
+  coachCalendar.render()
+}
 
-// const initDragAndDrop = () => {
-//   const containerEl = document.getElementById('external-events');
+const initDragAndDrop = () => {
+  const containerEl = document.getElementById('external-events');
 
-//   new Draggable(containerEl, {
-//     itemSelector: '.fc-event',
-//     eventData: function (eventEl) {
-//       return {
-//         title: eventEl.innerText
-//       };
-//     }
-//   });
-// }
+  new Draggable(containerEl, {
+    itemSelector: '.fc-event',
+    eventData: function (eventEl) {
+      return {
+        title: eventEl.innerText
+      };
+    }
+  });
+}
 
 const initCoachCalendar = () => {
   console.log("je suis initCoachCalendar")
-  // coachCalendarEl = document.getElementById('coach_calendar');
-  // if (coachCalendarEl) {
-  //   coachEvents = JSON.parse(coachCalendarEl.dataset.events);
-  //   createCoachCalendar()
-  //   initDragAndDrop()
-  // }
+  coachCalendarEl = document.getElementById('coach_calendar');
+  if (coachCalendarEl) {
+    coachEvents = JSON.parse(coachCalendarEl.dataset.events);
+    createCoachCalendar()
+    initDragAndDrop()
+  }
 }
 
 export { initCoachCalendar }
