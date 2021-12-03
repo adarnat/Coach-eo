04bfb0f638983d030863f6cfc3ab16838a5d5905
amelie darnat <darnat.amelie@gmail.com>
2021-12-03 15:17:25 +0100

    ad ajax on update booking payment

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,13 +2,6 @@ class BookingsController < ApplicationController
 
   def update
     @booking = Booking.find(params[:id])
-    if @booking.update(payment_received: params[:choice])
-      redirect_to client_profile_path(@booking.client_id), notice: "Bien mis à jour" if params[:redirect] == "client"
-      redirect_to root_path, notice: "Bien mis à jour" if params[:redirect] != "client"
-    else
-      redirect_to client_profile_path(@booking.client_id), notice: @booking.errors.messages if params[:redirect] == "client"
-      redirect_to root_path, notice: @booking.errors.messages if params[:redirect] != "client"
-    end
+    @booking.update(payment_received: params[:choice])
   end
-
 end
